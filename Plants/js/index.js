3a3309588f16e072c06c0fffc81bcb77638f4e0d
Dmitry Strizhakov <d.strizhakov@gmail.com>
2022-12-26 15:32:10 +0300

    feat: add burger menu

---
+++ Plants/js/index.js
@@ -0,0 +1,26 @@
+
+/************************************************/
+// Бургер меню //
+/************************************************/
+
+const menuIcon = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+
+if (menuIcon) {
+	document.body.addEventListener("click", function (e) {
+		// если кликнули по иконке бургер - переключаем классы
+		// открываем меню или закрываем его
+		if (e.target.closest('.menu__icon')) {
+			document.body.classList.toggle('_lock');
+			menuIcon.classList.toggle('_active');
+			menuBody.classList.toggle('_active');
+			// иначе кликнули вне иконки бургер - закрываем меню
+			// не важно, кникнули ли мы по пункту меню или вне его
+		} else {
+			document.body.classList.remove('_lock');
+			menuIcon.classList.remove('_active');
+			menuBody.classList.remove('_active');
+		};
+	});
+}
+
