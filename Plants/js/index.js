017c811e96185a3ecccc06756ac3288653c0a1c1
Dmitry Strizhakov <d.strizhakov@gmail.com>
2023-01-27 23:01:58 +0300

    feat: add services js code for blur

--- Plants/js/index.js
+++ Plants/js/index.js
@@ -36,8 +36,6 @@ if (menuIcon) {
 document.querySelectorAll('.accordeon-button').forEach((el) => {
 	// добавляем слушатель события "click" каждому элементу .accordeon-button
 	el.addEventListener('click', function (e) {
-		// на всякий случай прерываем действие браузера по умолчанию
-		e.preventDefault();
 		// получаем коллекцию элементов с классом .accordeon-item
 		// удаляем класс _active у всех элементов
 		document.querySelectorAll('.accordeon-item').forEach((el) => {
@@ -49,6 +47,112 @@ document.querySelectorAll('.accordeon-button').forEach((el) => {
 	});
 })
 /************************************************/
+// Обработка кнопок секции services, блюр//
+/************************************************/
+//обрабатывать будет родительский элемент кнопок через всплытие
+const serviceButtons = document.querySelector('.service__buttons');
+//карточки на которыми нужно управлять
+const plantingElements = document.querySelectorAll('.planting');
+const lawnElements = document.querySelectorAll('.lawn');
+const gardensElements = document.querySelectorAll('.gardens');
+
+//тут будет храниться состояние кнопок и соответственно состояние карточек
+let activeServicesArray = [];
+
+const changeServices = (e) => {
+	if (e.target.closest('.planting__btn')) {
+		// Если кликнули по кнопке .planting__btn, добавим в массив класс, если его там еще нет.
+		(activeServicesArray.join('').includes('.planting'))
+			// Класс уже есть в массиве, значит пользователь нажимает на активную кнопку.
+			? activeServicesArray = activeServicesArray.filter(el => el !== '.planting')
+			: activeServicesArray.push('.planting')
+	} else if (e.target.closest('.gardens__btn')) {
+		// Если кликнули по кнопке .gardens__btn, добавим в массив класс, если его там еще нет.
+		(activeServicesArray.join('').includes('.gardens'))
+			? activeServicesArray = activeServicesArray.filter(el => el !== '.gardens')
+			: activeServicesArray.push('.gardens')
+	} else if (e.target.closest('.lawn__btn')) {
+		// Если кликнули по кнопке .lawn__btn, добавим в массив класс, если его там еще нет.
+		(activeServicesArray.join('').includes('.lawn'))
+			? activeServicesArray = activeServicesArray.filter(el => el !== '.lawn')
+			: activeServicesArray.push('.lawn')
+	} else {
+		//Если кликнули не по кнопке снимем блюр со всех элементов
+		activeServicesArray = [];
+	}
+	// Если активных кнопок больше 2х сбросим все активные кнопки.
+	if (activeServicesArray.length > 2) {
+		activeServicesArray = [];
+	}
+	// проставим классы на кнопках согласно состоянию массива активных классов activeServicesArray
+	setButtons();
+	// проставим классы на карточках согласно состоянию массива активных классов activeServicesArray
+	// если массив пустой уберем все блюры с карточек
+	activeServicesArray.length ? setServices() : setAllServices()
+}
+const setButtons = () => {
+	resetButtons();
+	activeServicesArray.forEach(className => document.querySelector(`${className}__btn`).classList.add('_active'))
+}
+const setServices = () => {
+	resetServices();
+	activeServicesArray.forEach(className => {
+		switch (className) {
+			case '.planting':
+				plantingElements.forEach(el => {
+					el.classList.remove('_blur');
+				})
+				break;
+			case '.gardens':
+				gardensElements.forEach(el => {
+					el.classList.remove('_blur');
+				})
+				break;
+			case '.lawn':
+				lawnElements.forEach(el => {
+					el.classList.remove('_blur');
+				})
+				break;
+			default:
+				console.log(`No element to change! Array is: ${activeServicesArray}`);
+		}
+	})
+}
+const resetServices = () => {
+	plantingElements.forEach(el => {
+		el.classList.add('_blur');
+	})
+	lawnElements.forEach(el => {
+		el.classList.add('_blur');
+	})
+	gardensElements.forEach(el => {
+		el.classList.add('_blur');
+	})
+}
+const setAllServices = () => {
+	plantingElements.forEach(el => {
+		el.classList.remove('_blur');
+	})
+	lawnElements.forEach(el => {
+		el.classList.remove('_blur');
+	})
+	gardensElements.forEach(el => {
+		el.classList.remove('_blur');
+	})
+}
+const resetButtons = () => {
+	document.querySelector('.planting__btn').classList.remove('_active');
+	document.querySelector('.gardens__btn').classList.remove('_active');
+	document.querySelector('.lawn__btn').classList.remove('_active');
+}
+// если кнопки существуют, будем слушать body, и определять нажатие на кнопки через всплытие
+if (serviceButtons) {
+	document.body.addEventListener("click", function (e) {
+		changeServices(e);
+	})
+}
+
+/************************************************/
 // Обработка кнопки выбора города (селект)//
 /************************************************/
 const buttonCity = document.querySelector('.button-city')
@@ -57,7 +161,7 @@ const selectContact = document.querySelector('.select-contact')
 const contactsImage = document.querySelector('.contacts__image')
 if (buttonCity) {
 	document.body.addEventListener("click", function (e) {
-		e.preventDefault();
+
 		if (e.target.closest('.button-city')) {
 			buttonCity.classList.toggle('_active');
 		} else {
